b6dc601c5cac1f154420abbaae493bb50d6d7c99
Eduardo dos Santos Almeida <digdins@gmail.com>
2021-09-19 19:39:10 -0300

    Set Golem structure

--- R/app_server.R
+++ R/app_server.R
@@ -5,6 +5,14 @@
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
-  # Your application server logic 
+  # Your application server logic
+  
+  selected_vessel <- callModule(mod_Selected_vessel_server, "Selected_vessel_ui_1", marine_dataset)
+  
+  callModule(mod_leaflet_map_server, "leaflet_map_ui_1", selected_vessel)
+  
+  callModule(mod_dashboard_cards_server, "dashboard_cards_ui_1", selected_vessel)
+  
+  callModule(mod_date_info_server, "date_info_ui_1", selected_vessel)
   
 }
